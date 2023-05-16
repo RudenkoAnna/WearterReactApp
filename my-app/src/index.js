ee80fea79e69c8c541a22d54897cc1113d75ee3f
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-05-16 14:50:03 +0300

    created some files

---
+++ my-app/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import WeatherSearch from "./WeatherSearch";
+import "./App.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+    </div>
+  </StrictMode>
+);
